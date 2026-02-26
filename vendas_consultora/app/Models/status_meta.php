ceacd020899ceaa14375a33b91a9a9e690943242
Alewesley-Sousa <alewesley1234@gmail.com>
2026-02-26 15:54:23 +0000

    [FLEAT]criação de tabelas de status e tipo

---
+++ vendas_consultora/app/Models/status_meta.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class status_meta extends Model
+{
+    //
+}
---
+++ vendas_consultora/database/seeders/TipoMovimentacaoEstoqueSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TipoMovimentacaoEstoqueSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
