0d4010ed346ead60c465377cb0d8cc57bc3e5210
Alewesley-Sousa <alewesley1234@gmail.com>
2026-03-01 19:46:26 -0300

    [FLEAT] criação dos arquivos que compõem a tabela estoques

---
+++ vendas_consultora/app/Http/Controllers/EstoquesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\estoques;
+use Illuminate\Http\Request;
+
+class EstoquesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(estoques $estoques)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(estoques $estoques)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, estoques $estoques)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(estoques $estoques)
+    {
+        //
+    }
+}
