89cdd6497e5541caaccb9cc51ef51f3f7a0d49ca
Alewesley-Sousa <alewesley1234@gmail.com>
2026-03-02 12:26:34 -0300

    [FLEAT] foi criado os arquivos referentes a tabela movimentacao_estoque

---
+++ vendas_consultora/app/Http/Controllers/MovimentacaoEstoqueController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\movimentacao_estoque;
+use Illuminate\Http\Request;
+
+class MovimentacaoEstoqueController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(movimentacao_estoque $movimentacao_estoque)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(movimentacao_estoque $movimentacao_estoque)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, movimentacao_estoque $movimentacao_estoque)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(movimentacao_estoque $movimentacao_estoque)
+    {
+        //
+    }
+}
