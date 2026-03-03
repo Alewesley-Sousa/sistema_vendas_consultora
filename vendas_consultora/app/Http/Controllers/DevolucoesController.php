bd2cf58b426218090eb8014dd839cecf9a172d03
Alewesley-Sousa <alewesley1234@gmail.com>
2026-03-03 11:07:21 -0300

    [FLEAT] criação dos arquivos referentes a tabela devoluções

---
+++ vendas_consultora/app/Http/Controllers/DevolucoesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\devolucoes;
+use Illuminate\Http\Request;
+
+class DevolucoesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(devolucoes $devolucoes)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(devolucoes $devolucoes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, devolucoes $devolucoes)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(devolucoes $devolucoes)
+    {
+        //
+    }
+}
