2f95a0d2754968de1dea53e2318d97872bfc1b1f
Alewesley-Sousa <alewesley1234@gmail.com>
2026-03-03 16:07:16 -0300

    [FLEAT] foram criados os arquivos referentes a tabela itens devolução

---
+++ vendas_consultora/app/Http/Controllers/ItensDevolucaoController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\itens_devolucao;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ItensDevolucaoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(itens_devolucao $itens_devolucao)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(itens_devolucao $itens_devolucao)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, itens_devolucao $itens_devolucao)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(itens_devolucao $itens_devolucao)
+    {
+        //
+    }
+}
