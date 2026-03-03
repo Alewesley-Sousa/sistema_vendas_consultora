4970f541483eae6a0e4207ff8a912245f84517ec
nathan-barros <nathan.barros1@aluno.ce.gov.br>
2026-03-03 15:19:46 -0300

    Dados Iniciais da tabela  StatusCatalogoSeeder.php

--- vendas_consultora/database/seeders/statusSeeders/StatusCatalogoSeeder.php
+++ vendas_consultora/database/seeders/statusSeeders/StatusCatalogoSeeder.php
@@ -2,13 +2,13 @@
 /**
  * Autor: Alewesley-Sousa (criador) && Nathan-Barros (desenvolvedor)
  * Data: 01/03/2026
- * Descrição: seeder responsavel por criar dados iniciais da tabela referente
+ * Descrição: seeder responsavel por criar dados iniciais da tabela status_catalogo
  */
 
 namespace Database\Seeders\statusSeeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class StatusCatalogoSeeder extends Seeder
 {
@@ -17,6 +17,10 @@ class StatusCatalogoSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+          DB::table('status_catalogo')->insert([
+            ['nome' => 'Ativo', 'descricao' => 'Catálogo ativo e disponível'],
+            ['nome' => 'Inativo', 'descricao' => 'Catálogo inativo e indisponível'],
+        ]);
+    }
     }
 }
