81977f317ab88972857c51d93806bebb544b919c
Alewesley-Sousa <alewesley1234@gmail.com>
2026-03-04 14:18:51 -0300

    [FLEAT] criado o controller que interaje com a tabela itens promocao

---
+++ vendas_consultora/app/Http/Controllers/ItensPromocaoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\itens_promocao;
+use Illuminate\Http\Request;
+
+class ItensPromocaoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(itens_promocao $itens_promocao)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(itens_promocao $itens_promocao)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, itens_promocao $itens_promocao)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(itens_promocao $itens_promocao)
+    {
+        //
+    }
+}
