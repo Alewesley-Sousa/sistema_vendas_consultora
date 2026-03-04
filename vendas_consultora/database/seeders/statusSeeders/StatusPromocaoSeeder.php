f1236961057fa9664cdcab616171f84752a4e53c
nathan-barros <nathan.barros1@aluno.ce.gov.br>
2026-03-04 10:45:36 -0300

    [FIX] Remoção de verificação de registros da tabela StatusPromocaoSeeder.php

--- vendas_consultora/database/seeders/statusSeeders/StatusPromocaoSeeder.php
+++ vendas_consultora/database/seeders/statusSeeders/StatusPromocaoSeeder.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Autor: Alewesley-Sousa (criador) && Nathan-Barros (desenvolvedor)
  * Data: 01/03/2026
@@ -17,28 +18,25 @@ class StatusPromocaoSeeder extends Seeder
      */
     public function run(): void
     {
-        // Verifica se já existem registros para não duplicar
-        if (DB::table('status_promocao')->count() === 0) {
-            DB::table('status_promocao')->insert([
-                [
-                    'nome' => 'Ativa', 
-                    'descricao' => 'Promoção ativa',
-                    'created_at' => now(),
-                    'updated_at' => now()
-                ],
-                [
-                    'nome' => 'Inativa', 
-                    'descricao' => 'Promoção inativa',
-                    'created_at' => now(),
-                    'updated_at' => now()
-                ],
-                [
-                    'nome' => 'Expirada', 
-                    'descricao' => 'Promoção expirada',
-                    'created_at' => now(),
-                    'updated_at' => now()
-                ]
-            ]);
-        }
+        DB::table('status_promocao')->insert([
+            [
+                'nome' => 'Ativa', 
+                'descricao' => 'Promoção ativa',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'nome' => 'Inativa', 
+                'descricao' => 'Promoção inativa',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'nome' => 'Expirada', 
+                'descricao' => 'Promoção expirada',
+                'created_at' => now(),
+                'updated_at' => now()
+            ]
+        ]);
     }
 }
