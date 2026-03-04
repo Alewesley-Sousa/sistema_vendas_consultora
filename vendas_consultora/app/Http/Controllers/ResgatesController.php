3872f7a78b04f09393deabdbac6c0eda91bca008
Alewesley-Sousa <alewesley1234@gmail.com>
2026-03-04 09:05:13 -0300

    [FLEAT] criado o controller referente da tabela resgates

---
+++ vendas_consultora/app/Http/Controllers/ResgatesController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\resgates;
+use Illuminate\Http\Request;
+
+class ResgatesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(resgates $resgates)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(resgates $resgates)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, resgates $resgates)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(resgates $resgates)
+    {
+        //
+    }
+}
