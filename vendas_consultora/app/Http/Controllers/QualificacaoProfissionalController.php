672876dc0749f78c097d8b2b362c7c50e9decd4b
Alewesley-Sousa <alewesley1234@gmail.com>
2026-03-05 19:44:47 -0300

    [FLEAT] criado o arquivo controller da tabela qualificação profissional.

---
+++ vendas_consultora/app/Http/Controllers/QualificacaoProfissionalController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\qualificacao_profissional;
+use Illuminate\Http\Request;
+
+class QualificacaoProfissionalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(qualificacao_profissional $qualificacao_profissional)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(qualificacao_profissional $qualificacao_profissional)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, qualificacao_profissional $qualificacao_profissional)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(qualificacao_profissional $qualificacao_profissional)
+    {
+        //
+    }
+}
