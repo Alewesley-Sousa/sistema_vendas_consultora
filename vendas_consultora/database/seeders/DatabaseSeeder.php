dc3ec620c756fb4cc815c87af8b3ce636d231c67
Alewesley-Sousa <alewesley1234@gmail.com>
2026-03-10 18:21:55 -0300

    [FLEAT] foi acrescentado o codigo que chama a classe hisorico cargo

--- vendas_consultora/database/seeders/DatabaseSeeder.php
+++ vendas_consultora/database/seeders/DatabaseSeeder.php
@@ -40,6 +40,8 @@ class DatabaseSeeder extends Seeder
             MetasSeeder::class,
             LogsSeeder::class,
             QualificacaoProfissionalSeeder::class,
+            HistoricoCargoSeeder::class,
+            
 
         ]);
     }
