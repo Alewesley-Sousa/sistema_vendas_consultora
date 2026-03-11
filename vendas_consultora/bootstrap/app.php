16667d91adba1a19fbdcbdb5f22c2130bd8e7784
Alewesley-Sousa <alewesley1234@gmail.com>
2026-03-11 15:00:50 -0300

    [FLEAT] foi criado o codigo para chama a api de rotas

--- vendas_consultora/bootstrap/app.php
+++ vendas_consultora/bootstrap/app.php
@@ -17,7 +17,11 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        //
+            // Registrar middlewares de rota (aliases)
+        $middleware->alias([
+            'cargo' => \App\Http\Middleware\CargoMiddleware::class,
+        ]);
+
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
