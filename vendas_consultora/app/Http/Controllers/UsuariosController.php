c83605ef07c7cb7ca8fc2b9d0884c0c2749cf270
Alewesley-Sousa <alewesley1234@gmail.com>
2026-03-10 22:00:39 -0300

    [FLEAT] adição de novas funcionalidades nos métodos: store, update, show, index, destroy

--- vendas_consultora/app/Http/Controllers/UsuariosController.php
+++ vendas_consultora/app/Http/Controllers/UsuariosController.php
@@ -9,15 +9,19 @@ namespace App\Http\Controllers;
 
 use App\Models\usuarios;
 use Illuminate\Http\Request;
+use App\Services\UsuarioService;
+use App\Http\Requests\UsuariosRequest;
 
 class UsuariosController extends Controller
 {
+
+    public function __construct(protected UsuarioService $service) {}
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        return response()->json($this->service->listar());
     }
 
     /**
@@ -25,23 +29,28 @@ class UsuariosController extends Controller
      */
     public function create()
     {
-        //
+        // vai ficar o codigo que leva a pagina de criar usuarios
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(UsuarioRequest $request): JsonResponse
     {
-        //
-    }
+        // O UsuarioRequest já validou os dados aqui
+        $usuario = $this->service->store($request->validated());
 
+        return response()->json([
+            'message' => 'Usuário criado com sucesso!',
+            'data' => $usuario
+        ], 210);
+    }
     /**
      * Display the specified resource.
      */
     public function show(usuarios $usuarios)
     {
-        //
+        return response()->json($user);
     }
 
     /**
@@ -55,16 +64,26 @@ class UsuariosController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, usuarios $usuarios)
+    public function update(UsuarioRequest $request, $id): JsonResponse
     {
-        //
+        // O Request validado garante que os dados estão corretos
+        $usuario = $this->usuarioService->update($id, $request->validated());
+
+        return response()->json([
+            'message' => 'Usuário atualizado com sucesso!',
+            'data' => $usuario
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(usuarios $usuarios)
+    public function destroy($id): JsonResponse
     {
-        //
+        $this->usuarioService->destroy($id);
+    
+        return response()->json([
+            'message' => 'Usuário removido com sucesso!'
+        ], 200);
     }
 }
