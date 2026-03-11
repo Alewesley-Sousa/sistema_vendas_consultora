8adeed73e19d43733d0b6794bfd64430642e0f8b
Alewesley-Sousa <alewesley1234@gmail.com>
2026-03-11 15:01:05 -0300

    [TEST] TEST

--- vendas_consultora/routes/web.php
+++ vendas_consultora/routes/web.php
@@ -4,3 +4,19 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\AutenticacaoController;
+
+Route::get('/login', [AutenticacaoController::class, 'showLogin'])->name('login');
+Route::post('/login', [AutenticacaoController::class, 'login']);
+Route::post('/logout', [AutenticacaoController::class, 'logout'])->name('logout');
+
+// Rotas protegidas por cargo
+Route::get('/distribuidora/dashboard', fn() => view('distribuidora.dashboard')); 
+   // ->middleware(['auth', 'cargo:distribuidora']);
+
+Route::get('/lider/dashboard', fn() => view('lider.dashboard'));
+    // ->middleware(['auth', 'cargo:lider']);
+
+Route::get('/consultora/dashboard', fn() => view('consultora.dashboard'));
+    // ->middleware(['auth', 'cargo:consultora']);
+
