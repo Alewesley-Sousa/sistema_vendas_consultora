a64aa8fdbf6cc5d5cdff80fbbc8f266b232cdc7e
Alewesley-Sousa <alewesley1234@gmail.com>
2026-03-11 22:45:12 -0300

    [FLEAT] acrescentado rotas para as paginas de recuperação de senha

--- vendas_consultora/routes/web.php
+++ vendas_consultora/routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\AutenticacaoController;
+use App\Http\Controllers\ResetarSenhaController;
 
 Route::get('/login', [AutenticacaoController::class, 'showLogin'])->name('login');
 Route::post('/login', [AutenticacaoController::class, 'login']);
@@ -20,3 +21,10 @@ Route::get('/lider/dashboard', fn() => view('lider.dashboard'));
 Route::get('/consultora/dashboard', fn() => view('consultora.dashboard'));
     // ->middleware(['auth', 'cargo:consultora']);
 
+Route::get('/recuperar-senha', [ResetarSenhaController::class, 'formularioRecuperacao'])->name('senha-formulario');
+
+Route::post('/recuperar-senha', [ResetarSenhaController::class, 'enviarLinkResetar'])->name('senha-email');
+
+Route::get('/resetar-senha/{token}', [ResetarSenhaController::class, 'formularioAtualizarSenha'])->name('senha.resetar');
+
+Route::post('/resetar-senha', [ResetarSenhaController::class, 'atualizarSenha'])->name('senha.update');
