b6bf93c2a73e5bd0639a009727ec4885938c952f
nathanbarros <nathan.barros1@aluno.ce.gov.br>
2026-03-16 13:35:18 -0300

    [FLEAT] inseção de dados em CatalogoSeeder.php

--- vendas_consultora/database/seeders/CatalogosSeeder.php
+++ vendas_consultora/database/seeders/CatalogosSeeder.php
@@ -13,14 +13,30 @@ class CatalogosSeeder extends Seeder
      */
     public function run(): void
     {
-        $catalogos = [
+         $catalogos = [
             [
-                'nome' => 'exemplo',
+                'nome' => 'Catálogo Verão 2026',
+                'tipo_catalogo_id' => 2,
+                'status_id' => 1,
+                'descricao' => 'Produtos de beleza para o verão',
+                'data_publicacao' => '2026-01-10',
+                'data_encerramento' => '2026-03-31'
+            ],
+            [
+                'nome' => 'Catálogo Pontos Bronze',
                 'tipo_catalogo_id' => 1,
+                'status_id' => 1,
+                'descricao' => 'Resgate de pontos nível bronze',
+                'data_publicacao' => '2026-01-15',
+                'data_encerramento' => '2026-06-30'
+            ],
+            [
+                'nome' => 'Catálogo Especial Dia das Mães',
+                'tipo_catalogo_id' => 2,
                 'status_id' => 2,
-                'descricao' => 'texto descritivo',
-                'data_encerramento' => '2026-12-12',
-                'data_publicacao' => '2026-03-02'
+                'descricao' => 'Promoções especiais para o Dia das Mães',
+                'data_publicacao' => '2026-04-20',
+                'data_encerramento' => '2026-05-15'
             ]
         ];
 
