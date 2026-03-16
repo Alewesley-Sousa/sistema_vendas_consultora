54c111745a4bcf92b0788201e851c2434ff2b6eb
nathanbarros <nathan.barros1@aluno.ce.gov.br>
2026-03-16 14:38:39 -0300

    [FLEAT] Inseção de dados no ItensDevolucaoSeeder.php

--- vendas_consultora/database/seeders/ItensDevolucaoSeeder.php
+++ vendas_consultora/database/seeders/ItensDevolucaoSeeder.php
@@ -13,28 +13,94 @@ class ItensDevolucaoSeeder extends Seeder
      */
     public function run(): void
     {
-        $itens = [
+        $itensDevolucao = [
             // Devolução 9 (pedido 1, parcial)
-            ['item_pedido_id' => null, 'devolucao_id' => null, 'quantidade' => 1, 'subtotal' => 39.90],
-            ['item_pedido_id' => null, 'devolucao_id' => null, 'quantidade' => 1, 'subtotal' => 59.90],
+            [
+                'item_pedido_id' => 1,
+                'devolucao_id' => 9,
+                'quantidade' => 1,
+                'subtotal' => 39.90
+            ],
+            [
+                'item_pedido_id' => 2,
+                'devolucao_id' => 9,
+                'quantidade' => 1,
+                'subtotal' => 59.90
+            ],
 
             // Devolução 10 (pedido 2, total)
-            ['item_pedido_id' => null, 'devolucao_id' => null, 'quantidade' => 1, 'subtotal' => 89.90],
+            [
+                'item_pedido_id' => 3,
+                'devolucao_id' => 10,
+                'quantidade' => 1,
+                'subtotal' => 89.90
+            ],
 
             // Devolução 11 (pedido 3, parcial)
-            ['item_pedido_id' => null, 'devolucao_id' => null, 'quantidade' => 1, 'subtotal' => 79.90],
-            ['item_pedido_id' => null, 'devolucao_id' => null, 'quantidade' => 1, 'subtotal' => 69.90],
+            [
+                'item_pedido_id' => 4,
+                'devolucao_id' => 11,
+                'quantidade' => 1,
+                'subtotal' => 79.90
+            ],
+            [
+                'item_pedido_id' => 5,
+                'devolucao_id' => 11,
+                'quantidade' => 1,
+                'subtotal' => 69.90
+            ],
 
             // Devolução 12 (pedido 4, total)
-            ['item_pedido_id' => null, 'devolucao_id' => null, 'quantidade' => 1, 'subtotal' => 45.90],
-            ['item_pedido_id' => null, 'devolucao_id' => null, 'quantidade' => 1, 'subtotal' => 129.90],
+            [
+                'item_pedido_id' => 6,
+                'devolucao_id' => 12,
+                'quantidade' => 1,
+                'subtotal' => 45.90
+            ],
+            [
+                'item_pedido_id' => 7,
+                'devolucao_id' => 12,
+                'quantidade' => 1,
+                'subtotal' => 129.90
+            ],
 
             // Devolução 13 (pedido 5, parcial)
-            ['item_pedido_id' => null, 'devolucao_id' => null, 'quantidade' => 1, 'subtotal' => 139.90],
+            [
+                'item_pedido_id' => 8,
+                'devolucao_id' => 13,
+                'quantidade' => 1,
+                'subtotal' => 139.90
+            ],
 
             // Devolução 14 (pedido 6, total)
-            ['item_pedido_id' => null, 'devolucao_id' => null, 'quantidade' => 1, 'subtotal' => 34.90],
-            ['item_pedido_id' => null, 'devolucao_id' => null, 'quantidade' => 1, 'subtotal' => 36.90]
+            [
+                'item_pedido_id' => 9,
+                'devolucao_id' => 14,
+                'quantidade' => 1,
+                'subtotal' => 34.90
+            ],
+            [
+                'item_pedido_id' => 10,
+                'devolucao_id' => 14,
+                'quantidade' => 1,
+                'subtotal' => 36.90
+            ],
+
+            // Devolução 15 (pedido 7, parcial)
+            [
+                'item_pedido_id' => 11,
+                'devolucao_id' => 15,
+                'quantidade' => 1,
+                'subtotal' => 49.90
+            ],
+
+            // Devolução 16 (pedido 9, total)
+            [
+                'item_pedido_id' => 12,
+                'devolucao_id' => 16,
+                'quantidade' => 1,
+                'subtotal' => 38.70
+            ]
         ];
 
         foreach ($itens as $item) {
