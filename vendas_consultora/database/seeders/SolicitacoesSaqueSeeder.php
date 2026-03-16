a4557da0c59ac462067345942ff3365053633657
nathan-barros <nathan.barros1@aluno.ce.gov.br>
2026-03-16 19:51:39 -0300

    [FLEAT] Inseção de dados no SolicitacoesSaqueSeeder.php

--- vendas_consultora/database/seeders/SolicitacoesSaqueSeeder.php
+++ vendas_consultora/database/seeders/SolicitacoesSaqueSeeder.php
@@ -15,10 +15,118 @@ class SolicitacoesSaqueSeeder extends Seeder
     {
         $solicitacoes = [
             [
-                'consultora_id' => 1,
-                'valor_solicitado' => 10.0,
+                'consultora_id' => 2,
+                'valor_solicitado' => 150.00,
                 'status_id' => 1,
-                'data_decisao' => '2026-12-12 12:60:60'
+                'data_decisao' => null
+            ],
+            [
+                'consultora_id' => 3,
+                'valor_solicitado' => 200.00,
+                'status_id' => 2,
+                'data_decisao' => now()
+            ],
+            [
+                'consultora_id' => 4,
+                'valor_solicitado' => 120.00,
+                'status_id' => 3,
+                'data_decisao' => now()
+            ],
+            [
+                'consultora_id' => 5,
+                'valor_solicitado' => 300.00,
+                'status_id' => 2,
+                'data_decisao' => now()
+            ],
+            [
+                'consultora_id' => 6,
+                'valor_solicitado' => 180.00,
+                'status_id' => 1,
+                'data_decisao' => null
+            ],
+            [
+                'consultora_id' => 7,
+                'valor_solicitado' => 250.00,
+                'status_id' => 2,
+                'data_decisao' => now()
+            ],
+            [
+                'consultora_id' => 8,
+                'valor_solicitado' => 90.00,
+                'status_id' => 3,
+                'data_decisao' => now()
+            ],
+            [
+                'consultora_id' => 9,
+                'valor_solicitado' => 400.00,
+                'status_id' => 2,
+                'data_decisao' => now()
+            ],
+            [
+                'consultora_id' => 10,
+                'valor_solicitado' => 220.00,
+                'status_id' => 1,
+                'data_decisao' => null
+            ],
+            [
+                'consultora_id' => 11,
+                'valor_solicitado' => 350.00,
+                'status_id' => 2,
+                'data_decisao' => now()
+            ],
+            [
+                'consultora_id' => 12,
+                'valor_solicitado' => 175.00,
+                'status_id' => 3,
+                'data_decisao' => now()
+            ],
+            [
+                'consultora_id' => 13,
+                'valor_solicitado' => 280.00,
+                'status_id' => 2,
+                'data_decisao' => now()
+            ],
+            [
+                'consultora_id' => 14,
+                'valor_solicitado' => 95.00,
+                'status_id' => 1,
+                'data_decisao' => null
+            ],
+            [
+                'consultora_id' => 15,
+                'valor_solicitado' => 310.00,
+                'status_id' => 2,
+                'data_decisao' => now()
+            ],
+            [
+                'consultora_id' => 16,
+                'valor_solicitado' => 140.00,
+                'status_id' => 3,
+                'data_decisao' => now()
+            ],
+            [
+                'consultora_id' => 17,
+                'valor_solicitado' => 260.00,
+                'status_id' => 2,
+                'data_decisao' => now()
+            ],
+            [
+                'consultora_id' => 18,
+                'valor_solicitado' => 330.00,
+                'status_id' => 1,
+                'data_decisao' => null
+            ],
+            [
+                'consultora_id' => 19,
+                'valor_solicitado' => 210.00,
+                'status_id' => 2,
+                'data_decisao' => now()
+            ],
+            [
+                'consultora_id' => 20,
+                'valor_solicitado' => 500.00,
+                'status_id' => 2,
+                'data_decisao' => now()
             ]
         ];
 
