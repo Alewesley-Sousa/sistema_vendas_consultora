9636d295b31aa3fb69b40819b07714cefc3c5a42
nathanbarros <nathan.barros1@aluno.ce.gov.br>
2026-03-16 14:34:21 -0300

    [FLEAT] Inseção de dados no ItensCatalogoSeeder.php

--- vendas_consultora/database/seeders/ItensCatalogoSeeder.php
+++ vendas_consultora/database/seeders/ItensCatalogoSeeder.php
@@ -13,13 +13,106 @@ class ItensCatalogoSeeder extends Seeder
     public function run(): void
     {
         $itensCatalogo = [
+            // Catálogo Verão 2026 (catalogo_id = 1) - vendas
             [
-                'preco' => 1223.00,
-                'pontos_necesarios' => 12123434,
+                'preco' => 39.90,
+                'pontos_necessarios' => null,
                 'status_id' => 1,
-                'estoque_disponivel' => 1432,
+                'estoque_disponivel' => 100,
                 'catalogo_id' => 1,
                 'produto_id' => 1
+            ],
+            [
+                'preco' => 59.90,
+                'pontos_necessarios' => null,
+                'status_id' => 1,
+                'estoque_disponivel' => 80,
+                'catalogo_id' => 1,
+                'produto_id' => 2
+            ],
+            [
+                'preco' => 89.90,
+                'pontos_necessarios' => null,
+                'status_id' => 2,
+                'estoque_disponivel' => 0,
+                'catalogo_id' => 1,
+                'produto_id' => 3
+            ],
+            [
+                'preco' => 79.90,
+                'pontos_necessarios' => null,
+                'status_id' => 1,
+                'estoque_disponivel' => 50,
+                'catalogo_id' => 1,
+                'produto_id' => 4
+            ],
+
+            // Catálogo Pontos Bronze (catalogo_id = 2) - recompensas
+            [
+                'preco' => 39.90,
+                'pontos_necessarios' => 400,
+                'status_id' => 1,
+                'estoque_disponivel' => 100,
+                'catalogo_id' => 2,
+                'produto_id' => 1
+            ],
+            [
+                'preco' => 59.90,
+                'pontos_necessarios' => 600,
+                'status_id' => 1,
+                'estoque_disponivel' => 80,
+                'catalogo_id' => 2,
+                'produto_id' => 2
+            ],
+            [
+                'preco' => 129.90,
+                'pontos_necessarios' => 1300,
+                'status_id' => 1,
+                'estoque_disponivel' => 40,
+                'catalogo_id' => 2,
+                'produto_id' => 7
+            ],
+            [
+                'preco' => 139.90,
+                'pontos_necessarios' => 1400,
+                'status_id' => 2,
+                'estoque_disponivel' => 0,
+                'catalogo_id' => 2,
+                'produto_id' => 8
+            ],
+
+            // Catálogo Especial Dia das Mães (catalogo_id = 3) - vendas
+            [
+                'preco' => 34.90,
+                'pontos_necessarios' => null,
+                'status_id' => 1,
+                'estoque_disponivel' => 120,
+                'catalogo_id' => 3,
+                'produto_id' => 9
+            ],
+            [
+                'preco' => 36.90,
+                'pontos_necessarios' => null,
+                'status_id' => 1,
+                'estoque_disponivel' => 110,
+                'catalogo_id' => 3,
+                'produto_id' => 10
+            ],
+            [
+                'preco' => 49.90,
+                'pontos_necessarios' => null,
+                'status_id' => 2,
+                'estoque_disponivel' => 0,
+                'catalogo_id' => 3,
+                'produto_id' => 11
+            ],
+            [
+                'preco' => 12.90,
+                'pontos_necessarios' => null,
+                'status_id' => 1,
+                'estoque_disponivel' => 200,
+                'catalogo_id' => 3,
+                'produto_id' => 12
             ]
         ];
     }
