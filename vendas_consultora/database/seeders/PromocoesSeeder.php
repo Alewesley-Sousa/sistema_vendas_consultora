c5c887abf5959e9baab31c21beb9533a8a585da2
nathan-barros <nathan.barros1@aluno.ce.gov.br>
2026-03-16 19:39:16 -0300

    [FLEAT] Inseção de dados no PromocoesSeeder.php

--- vendas_consultora/database/seeders/PromocoesSeeder.php
+++ vendas_consultora/database/seeders/PromocoesSeeder.php
@@ -15,14 +15,59 @@ class PromocoesSeeder extends Seeder
     {
         $promocoes = [
             [
-                'nome' => 'exemplo',
-                'desconto' => 0.5,
-                'descricao' => 'exemplo de texto',
+                'nome' => 'Promoção Carnaval 2026',
+                'desconto' => 20.00,
+                'descricao' => 'Desconto de 20% em toda a linha de maquiagem',
                 'tipo_promocao_id' => 1,
-                'data_inicio' => '2026-05-12',
-                'data_fim' => '2026-08-29',
+                'data_inicio' => '2026-02-01',
+                'data_fim' => '2026-02-15',
                 'status_id' => 1
-           ]
+            ],
+            [
+                'nome' => 'Semana da Beleza',
+                'desconto' => 15.00,
+                'descricao' => 'Desconto fixo de R$15 em hidratantes faciais',
+                'tipo_promocao_id' => 2,
+                'data_inicio' => '2026-03-01',
+                'data_fim' => '2026-03-10',
+                'status_id' => 1
+            ],
+            [
+                'nome' => 'Brinde Dia das Mães',
+                'desconto' => 0.00,
+                'descricao' => 'Na compra de perfumes, leve um batom grátis',
+                'tipo_promocao_id' => 3,
+                'data_inicio' => '2026-04-20',
+                'data_fim' => '2026-05-15',
+                'status_id' => 2
+            ],
+            [
+                'nome' => 'Frete Grátis Outono',
+                'desconto' => 0.00,
+                'descricao' => 'Frete grátis em compras acima de R$100',
+                'tipo_promocao_id' => 4,
+                'data_inicio' => '2026-03-01',
+                'data_fim' => '2026-03-31',
+                'status_id' => 1
+            ],
+            [
+                'nome' => 'Black Friday 2026',
+                'desconto' => 50.00,
+                'descricao' => 'Desconto de 50% em produtos selecionados',
+                'tipo_promocao_id' => 1,
+                'data_inicio' => '2026-11-01',
+                'data_fim' => '2026-11-30',
+                'status_id' => 3
+            ],
+            [
+                'nome' => 'Natal Encantado',
+                'desconto' => 30.00,
+                'descricao' => 'Desconto fixo de R$30 em kits de presente',
+                'tipo_promocao_id' => 2,
+                'data_inicio' => '2026-12-01',
+                'data_fim' => '2026-12-25',
+                'status_id' => 1
+            ]
         ];
 
         foreach ($promocoes as $promocao) {
