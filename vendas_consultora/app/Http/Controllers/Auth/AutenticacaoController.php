5d89dc5de204f7975ca2a10c7671f732b5c0372a
Alewesley-Sousa <alewesley1234@gmail.com>
2026-03-17 22:33:06 -0300

    [FIX] alteração no nome de user para usuarios

--- vendas_consultora/app/Http/Controllers/Auth/AutenticacaoController.php
+++ vendas_consultora/app/Http/Controllers/Auth/AutenticacaoController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use App\Models\usuarios;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -27,6 +27,7 @@ class AutenticacaoController extends Controller
             $request->session()->regenerate();
 
             $user = Auth::user();
+
 
             // Redireciona conforme o cargo já existente na tabela
             switch ($user->cargo) {
