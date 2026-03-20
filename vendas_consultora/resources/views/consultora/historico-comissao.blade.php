0c1b9f9cbdd456d7770068b3cf7e620628bc42fb
Alewesley-Sousa <alewesley1234@gmail.com>
2026-03-20 18:48:38 -0300

    [TEST] pagina criada para teste do metodos traz o dado do historico de comissoes

---
+++ vendas_consultora/resources/views/consultora/historico-comissao.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+@section('conteudo')
+<div id='HistoricoComissao'>
+    <table>
+        <thead>
+            <ol>
+                <li>Id Pedido</li>
+                <li>Tipo Comissão</li>
+                <li>Valor</li>
+                <li>Tipo Movimentação</li>
+                <li>Data Movimentação</li>
+            </ol>
+        </thead>
+        <tbody>
+            <!-- inicio do foreach -->
+            <ol>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ol>
+            <!-- fim do foreach -->
+        </tbody>
+    </table>
+</div>
+@endsection
