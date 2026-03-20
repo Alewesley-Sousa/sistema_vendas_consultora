a906271cc596889c3fbc98acb0768894958fb89c
Alewesley-Sousa <alewesley1234@gmail.com>
2026-03-19 22:01:55 -0300

    [FLEAT] adicionado função de pre-loader (pré carregamento de pagina)

--- vendas_consultora/resources/js/app.js
+++ vendas_consultora/resources/js/app.js
@@ -6,13 +6,19 @@ import { initRecuperarSenha } from './pages/recuperar-senha';
 import { initAtualizarSenha } from './pages/atualizar-senha';
 import { initLogin } from './pages/login';
 import { initDashboardConsultora } from './pages/dashboard-consultora';
+import { UIService } from './service/uiService';
 
 // Inicializa a lógica da página
 document.addEventListener('DOMContentLoaded', () => {
+    UIService.init(); // Inicializa o elemento do loader
+
     initRecuperarSenha();
     initDashboardConsultora();
     initAtualizarSenha();
     initLogin();
+
+// Garante que o loader suma quando a página carregar (F5/Navegação normal)
+    UIService.hide();
 });
 window.ResetarSenhaService = ResetarSenhaService;
 window.AuthService = AuthService;
