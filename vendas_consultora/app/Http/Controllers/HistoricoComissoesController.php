8ff0f2cd70a0b1b1f745838004384dc8fb7b8b5d
Alewesley-Sousa <alewesley1234@gmail.com>
2026-03-20 18:46:24 -0300

    [FLEAT] deletado codigo pronto e implementado a função de visualizar o historico

--- vendas_consultora/app/Http/Controllers/HistoricoComissoesController.php
+++ vendas_consultora/app/Http/Controllers/HistoricoComissoesController.php
@@ -3,63 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Models\historico_comissoes;
+use App\Services\HistoricoComissaoService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HistoricoComissoesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    protected $historicoComissaoService;
+
+    public function __construct(HistoricoComissaoService $historicoComissao)
     {
-        //
+        $this->historicoComissaoService = $historicoComissao;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function visualizarHistorico()
     {
-        //
-    }
+        $idUsuario = Auth::id();
+        $historicoComissoes = $this->historicoComissaoService->PegarHistoricoComissao($idUsuario);
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(historico_comissoes $historico_comissoes)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(historico_comissoes $historico_comissoes)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, historico_comissoes $historico_comissoes)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(historico_comissoes $historico_comissoes)
-    {
-        //
+        return response()->json([
+            'status' => 'sucesso',
+            'data' => $historicoComissoes
+        ]);
     }
 }
