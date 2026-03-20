23e6a1168f3a33e8289fc8a3683ce2aab9d85a6d
Alewesley-Sousa <alewesley1234@gmail.com>
2026-03-20 18:50:33 -0300

    [FLEAT] criado codigo que ainda esta imcompleto pela falta de informação

---
+++ vendas_consultora/resources/js/pages/historico-comissao.js
@@ -0,0 +1,9 @@
+// resources/js/pages/historico-comissao.js
+import { HistoricoComissaoService } from '../api/historicoComissoes';
+
+export function initHistoricoComissao() {
+    const historicoComissao = document.getElementById('HistoricoComissao');
+    if (!historicoComissao) return;
+
+    
+}
