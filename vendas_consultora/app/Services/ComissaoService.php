87d53cd126ce9019e0112f344a22d27b439bdd63
Alewesley-Sousa <alewesley1234@gmail.com>
2026-03-21 13:53:22 -0300

    [REF] foi refeito para ser flexivel no proposito de ou visualizar ou para edição

--- vendas_consultora/app/Services/ComissaoService.php
+++ vendas_consultora/app/Services/ComissaoService.php
@@ -2,15 +2,23 @@
 namespace App\Services;
 
 use App\Models\comissoes;
+use App\Models\historico_comissoes;
+use App\Models\solicitacoes_saque;
 use Exception;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class ComissaoService 
 {
-    public function comissaoUsuario($idUsuario) {
-        return comissoes::find($idUsuario);
+    public function comissaoUsuario($idUsuario, $atualizarRegistro = false) {
+        $query = comissoes::where('consultora_id', $idUsuario);
+        // se o parâmetro atualizar registro vier como true, então vai trancar o registro para evitar atualizações simultâneas
+        if ($atualizarRegistro) {
+            $query->lockForUpdate();
+        }
+        return $query->first();
     }
+
 }
 
 
