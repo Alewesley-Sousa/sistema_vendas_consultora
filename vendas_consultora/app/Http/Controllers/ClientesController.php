76f02a904076eddb0a32745f513bad1e18788dc1
Alewesley-Sousa <alewesley1234@gmail.com>
2026-03-21 19:07:49 -0300

    [FLEAT] foi implementado os metodos que criam e atualizam a tabela cliente

--- vendas_consultora/app/Http/Controllers/ClientesController.php
+++ vendas_consultora/app/Http/Controllers/ClientesController.php
@@ -2,41 +2,65 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ClienteRequest;
 use App\Models\clientes;
+use App\Services\ClientesService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ClientesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    protected $clienteService;
+
+    public function __construct(ClientesService $clientesService)
     {
-        //
+        $this->clienteService = $clientesService;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function formulario($id = null) {
+        return view('formularios.formulario-cliente', ['id' => $id]);
+    }
+
+    public function cadastrarCliente(ClienteRequest $request)
     {
-        //
+
+        // O $request aqui já passou pela validação de CPF/Email único!
+        $resultado = $this->clienteService->armazenar($request);
+
+        if ($resultado['status'] === 'success') {
+            return response()->json($resultado, 200);
+        }
+
+        return response()->json($resultado, 400);
+
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function atualizarDados(ClienteRequest $request, $id)
     {
-        //
+        // O $request aqui já passou pela validação de CPF/Email único!
+        $resultado = $this->clienteService->editar($request, $id);
+
+        if ($resultado['status'] === 'success') {
+            return response()->json($resultado, 200);
+        }
+
+        return response()->json($resultado, 400);
+
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(clientes $clientes)
+    public function exibir(clientes $cliente): JsonResponse
     {
-        //
+        return response()->json([
+            'status' => 'success',
+            'data' => $cliente
+        ], 200);
     }
 
     /**
