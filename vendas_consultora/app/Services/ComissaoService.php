97193263f515383ce71ed3b37695d42be3e35c3a
Alewesley-Sousa <alewesley1234@gmail.com>
2026-03-21 14:03:22 -0300

    [FLEAT] foi criado a logica da solicitação de saque

--- vendas_consultora/app/Services/ComissaoService.php
+++ vendas_consultora/app/Services/ComissaoService.php
@@ -24,6 +24,54 @@ class ComissaoService
         return solicitacoes_saque::where('consultora_id', $usuario->id)->where('status_id', 1)->first();
     }
 
+
+    public function solicitarSaque() {
+        DB::beginTransaction(); // inicia a regra do saque
+        
+        try {
+            $usuario = Auth::user(); // pega os dados do usuario autenticado
+            $registroSaldo = $this->comissaoUsuario($usuario->id, true);
+
+            // verificar se existe uma solicitação 'pendente'.
+            $solicitacaoPendente = $this->solicitacaoPendente();
+
+            if ($solicitacaoPendente) {
+                throw new Exception("você ja possui solicitação pendente");
+            }
+            
+            // a transação para se não existir registro ou valor acima de zero
+            if (!$registroSaldo || $registroSaldo->saldo_liquido <= 0) {
+                throw new Exception("Saldo insuficiente ou inexistente para saque.");
+            }
+
+
+            // pega o valor total a sacar
+            $valorSolicitacao = $registroSaldo->saldo_liquido; 
+
+            solicitacoes_saque::create([
+                'consultora_id' => $usuario->id,
+                'valor_solicitado' => $valorSolicitacao,
+                'status_id' => 1,
+                'data_decisao' => null
+            ]);
+
+            DB::commit();
+            
+            return [
+                'status' => 'success',
+                'mensagem' => 'Saque solicitado com sucesso!',
+                'valor_solicitado' => $valorSolicitacao
+            ];
+
+        } catch (Exception $e) {
+            DB::rollBack();
+
+            return [
+                'status' => 'error',
+                'mensagem' => 'Falha ao solicitar o saque: ' . $e->getMessage()
+            ];
+        }
+    }
 }
 
 
