55ae3c7abd174df503c4897d2519d2feca8d1036
Alewesley-Sousa <alewesley1234@gmail.com>
2026-03-24 16:35:04 -0300

    [FIX] ajeitado

--- vendas_consultora/resources/js/app.js
+++ vendas_consultora/resources/js/app.js
@@ -2,31 +2,20 @@ import './bootstrap';
 
 import { AuthService } from './api/auth';
 import { ResetarSenhaService } from './api/resetarSenha';
-import { initRecuperarSenha } from './pages/recuperar-senha';
-import { initAtualizarSenha } from './pages/atualizar-senha';
+import { initRecuperarSenha } from './recuperar-senha'
+import { initAtualizarSenha } from './atualizar-senha';
 import { initLogin } from './pages/login';
-import { initDashboardConsultora } from './pages/dashboard-consultora';
-import { UIService } from './service/uiService';
-import { initHistoricoComissao } from "./pages/historico-comissao";
-import { initFormCliente } from './pages/formularios/formulario-cliente';
-import { initFormUsuario } from './pages/formularios/formulario-usuario';
-import { InitCatalogoProduto } from './pages/catalogo-produtos';
 
 // Inicializa a lógica da página
 document.addEventListener('DOMContentLoaded', () => {
-    UIService.init(); // Inicializa o elemento do loader
+    // UIService.init(); // Inicializa o elemento do loader
 
     initRecuperarSenha();
-    initDashboardConsultora();
     initAtualizarSenha();
     initLogin();
-    initHistoricoComissao();
-    initFormCliente();
-    initFormUsuario();
-    InitCatalogoProduto();
 
 // Garante que o loader suma quando a página carregar (F5/Navegação normal)
-    UIService.hide();
+    // UIService.hide();
 });
-window.ResetarSenhaService = ResetarSenhaService;
+window.ResetarSenhaSeResetarSenhaServiceetarSenhaService;
 window.AuthService = AuthService;
