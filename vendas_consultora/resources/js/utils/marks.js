7f62fc41d27267bb584a8cf5a10386bfb6969118
Alewesley-Sousa <alewesley1234@gmail.com>
2026-03-26 00:43:59 -0300

    [FLEAT] arquivo que adiciona metodo de mascara de dados

---
+++ vendas_consultora/resources/js/utils/marks.js
@@ -0,0 +1,25 @@
+export const Masks = {
+    cpf(value) {
+        return value
+            .replace(/\D/g, '')
+            .replace(/(\d{3})(\d)/, '$1.$2')
+            .replace(/(\d{3})(\d)/, '$1.$2')
+            .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+            .replace(/(-\d{2})\d+?$/, '$1');
+    },
+
+    telefone(value) {
+        return value
+            .replace(/\D/g, '')
+            .replace(/^(\d{2})(\d)/g, '($1) $2')
+            .replace(/(\d{5})(\d)/, '$1-$2')
+            .replace(/(-\d{4})\d+?$/, '$1');
+    },
+
+    cep(value) {
+        return value
+            .replace(/\D/g, '')
+            .replace(/^(\d{5})(\d)/, '$1-$2')
+            .replace(/(-\d{3})\d+?$/, '$1');
+    }
+};
