ab9918c4957c0db076349f46ca0ce87dac89e626
Alewesley-Sousa <alewesley1234@gmail.com>
2026-03-25 21:10:06 -0300

    [FLEAT] instalação do alpine e coloquei paleta de cores

--- vendas_consultora/tailwind.config.js
+++ vendas_consultora/tailwind.config.js
@@ -1,19 +1,27 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.jsx',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.jsx",
     ],
 
     theme: {
         extend: {
+            colors: {
+                "navy-petroleo": "#2C3E50", // Profissionalismo
+                "navy-coral": "#FF6F61", // Empoderamento
+                "navy-hot-pink": "#FF69B4", // Meta alcançada
+                "navy-gold": "#FFD700", // Sucesso e conquista
+                "navy-claro": "#FFF5F7", // Suavidade
+            },
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Rotis Sans Serif", "sans-serif"], // Tipografia principal
+                serif: ["The Seasons", "serif"], // Tipografia de destaque
             },
         },
     },
