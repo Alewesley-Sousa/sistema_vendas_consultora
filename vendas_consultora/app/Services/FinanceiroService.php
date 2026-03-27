524883536390db856073afddaf591fe365560f9b
Alewesley-Sousa <alewesley1234@gmail.com>
2026-03-27 12:50:20 -0300

    [FLEAT] falta terminar a logica do sistema

---
+++ vendas_consultora/app/Services/FinanceiroService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\itens_catalogo;
+use App\Models\pedidos;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class FinanceiroService
+{
+    /**
+     * Cria um novo pedido
+     */
+    public function CriarPedido(array $dados)
+    {
+        DB::beginTransaction();
+        try {
+            $todosProdutos = $dados['produtos'];
+            $idCliente = $dados['cliente_id'] ? $dados['cliente_id'] : null;
+            $idUsuario = Auth::id();
+
+            $pedido = pedidos::create([
+                'cliente_id' => $idCliente,
+                'usuario_id' => $idUsuario,
+                'produtos' => $todosProdutos
+            ]);
+
+
+
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+    public function procurarPromocao($id)
+    {
+        $produto = itens_catalogo::where('produto_id', $id)->first();
+        
+    }
+}
