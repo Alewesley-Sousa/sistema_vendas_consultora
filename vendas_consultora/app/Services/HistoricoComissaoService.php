b25bb5f09952862bae6d91a4afffaee9c045e410
Alewesley-Sousa <alewesley1234@gmail.com>
2026-03-29 00:15:00 -0300

    [FLEAT] foi criado o metodo que pega a comissão por mês e ja desconta as comissoes estornadas

--- vendas_consultora/app/Services/HistoricoComissaoService.php
+++ vendas_consultora/app/Services/HistoricoComissaoService.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+
 namespace App\Services;
 
 use App\Models\historico_comissoes;
@@ -9,36 +10,54 @@ use Symfony\Component\HttpFoundation\Request;
 
 class HistoricoComissaoService
 {
-    public function PegarHistoricoComissao(Request $request) {
+    public function PegarHistoricoComissao(Request $request)
+    {
         $usuario = Auth::user();
         $cargo = $usuario->cargo;
 
         $colunasPermitidas = ['valor', 'data_movimentacao'];
-        
+
         // 2. Captura os valores ou define um padrão (default)
         $coluna = in_array($request->ordenar_por, $colunasPermitidas) ? $request->ordenar_por : 'data_movimentacao';
         $direcao = ($request->direcao === 'asc') ? 'asc' : 'desc';
 
         return historico_comissoes::query()
-        ->with(
-            [
-               'tipoComissao', 'tipoMovimentacao' 
-            ]
-        )
-        //Se não for distribuidora, vai mostrar apenas o historico do usuario autenticado.
-        ->when($cargo !== 'distribuidora', function ($query) use ($usuario) {
-            return $query->where('consultora_id', $usuario->id);
-        })
+            ->with(
+                [
+                    'tipoComissao',
+                    'tipoMovimentacao'
+                ]
+            )
+            //Se não for distribuidora, vai mostrar apenas o historico do usuario autenticado.
+            ->when($cargo !== 'distribuidora', function ($query) use ($usuario) {
+                return $query->where('consultora_id', $usuario->id);
+            })
 
-        //Se for distribuidora, vai filtrar pelo id do usuario escolhido
-        ->when($cargo === 'distribuidora' && $request->id_usuario, function ($query, $idEscolhido) {
-            return $query->where('consultora_id', $idEscolhido);
-        })
-        ->whereIn('tipo_movimentacao_id', [1, 2]) // Só vai pegar do tipo estorno e venda
+            //Se for distribuidora, vai filtrar pelo id do usuario escolhido
+            ->when($cargo === 'distribuidora' && $request->id_usuario, function ($query, $idEscolhido) {
+                return $query->where('consultora_id', $idEscolhido);
+            })
+            ->whereIn('tipo_movimentacao_id', [1, 2]) // Só vai pegar do tipo estorno e venda
 
-        ->orderBy($coluna, $direcao)
-        ->paginate(10);
+            ->orderBy($coluna, $direcao)
+            ->paginate(10);
+    }
 
-
+    public function comissaoPorMes($id)
+    {
+        $comissaoVenda = historico_comissoes::where('consultora_id', $id)
+            ->selectRaw("
+        strftime('%m/%Y', data_movimentacao) as mes_referencia,
+        SUM(CASE 
+            WHEN tipo_movimentacao_id = 1 THEN valor 
+            WHEN tipo_movimentacao_id = 2 THEN -valor 
+            ELSE 0 
+        END) as valor_final
+    ")
+            ->groupBy('mes_referencia')
+            ->orderBy('mes_referencia', 'desc')
+            ->get();
+            
+        return $comissaoVenda;
     }
 }
