fd9d67978e006c603337a3445f9c5ae9b4181197
Alewesley-Sousa <alewesley1234@gmail.com>
2026-03-29 00:16:22 -0300

    [REF] pequena modificação que deixa o metodo reutilizavel por outros metodos

--- vendas_consultora/app/Services/LiderService.php
+++ vendas_consultora/app/Services/LiderService.php
@@ -2,19 +2,26 @@
 
 namespace App\Services;
 
+use App\Models\devolucoes;
+use App\Models\pedidos;
 use App\Models\usuarios;
 use Exception;
 use Illuminate\Support\Facades\Auth;
 
 class LiderService
 {
+    protected $HistoricoComissaoService;
+
+    public function __construct(HistoricoComissaoService $service) {
+        $this->HistoricoComissaoService = $service;
+    }
 
     /**
      * tras todas consultoras vinculadas diretamente.
      * consultoras que tem o id do lider na coluna 'consultora_id'
      */
 
-    public function consultorasVinculadas()
+    public function consultorasVinculadas($reutilizavel = false)
     {
         try {
             $usuario = Auth::user();
@@ -33,6 +40,14 @@ class LiderService
             if (!$resultado) {
                 throw new Exception("Nenhuma consultora encontrada");
             }
+
+            /**
+             * pegar apenas a query para reutilizar em outras funcoes
+             */
+            if ($reutilizavel === true) {
+                return $resultado;
+            }
+
 
             return [
                 'status' => 'success',
