0b049c87b691b06e9a5518eec6672012205dde5f
Alewesley-Sousa <alewesley1234@gmail.com>
2026-04-01 06:17:34 -0300

    [FLEAT] adicionado a logica que atualiza o pedido com auditoria

--- vendas_consultora/app/Services/PedidosService.php
+++ vendas_consultora/app/Services/PedidosService.php
@@ -7,6 +7,9 @@ use App\Models\pedidos;
 use Exception;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use App\Interfaces\calcularSubtotal;
+use App\Interfaces\calcularTotal;
+use App\Services\LogService;
 
 class PedidosService
 {
@@ -29,24 +32,54 @@ class PedidosService
             // apenas pode ser feita a atualização dos campos abaixo
 
             //tabela pedidos
-            $pedido->valor_total = $data['valor_total'] ?? $pedido->valor_total;
             $pedido->status_id = $data['status_id'] ?? $pedido->status_id;
             $pedido->tipo_pagamento = $data['tipo_pagamento'] ?? $pedido->tipo_pagamento;
 
             // tabela itens_pedido
-            collect($data['itens'])->map(function ($itemData) use ($itensPedidos) {
+            $itensAtualizados = collect($data['itens'])->map(function
+            ($itemData) use ($itensPedidos, $usuarioLongado) {
                 $item = $itensPedidos->firstWhere('produto_id', $itemData['produto_id']);
 
                 if ($item) {
                     $item->quantidade = $itemData['quantidade'] ?? $item->quantidade;
+                    if ($usuarioLongado->cargo == "distribuidora") {
+                    	$item->preco_unitario = $itemData['preco_unitario'] ??
+                    	$item->preco_unitario;
+                    }
+                    $calculador = new calcularSubtotal($item->quantidade,
+                    $item->preco_unitario);
+                    $item->subtotal = $calculador->calcular();
                     $item->save();
                 }
+                
+                return $item;
             });
-
+            $subtotais = $itensAtualizados->map(function ($item) {
+            	return $item->subtotal;
+            })->toArray();
+            $calculador = new calcularTotal($subtotais);
+            $pedido->valor_total = $calculador->calcular();
+            
             $pedido->save();
+            
+           LogService::registrarAcao(
+            	"Atualizar os produtos do pedido #$pedido->id",
+            	"Pedidos e itens_pedido",
+            	$pedido->id,
+            	"Atualizando os dados dos produtos de um pedido"
+            	);
+            DB::commit();
+            
+            return [
+            	'status' => 'success',
+            	'mensagem' => "os produtos do pedido #{$pedido->id} foram atualizados com sucesso"
+            ];
         } catch (Exception $e) {
             DB::rollBack();
-            throw $e;
+            return [
+            	'status' => 'error',
+            	'mensagem' => "houve um erro no sistema: " . $e->getMessage()
+            ];
         }
     }
 
@@ -57,7 +90,9 @@ class PedidosService
             if (!$usuarioLongado) {
                 throw new Exception('Acesso negado!');
             }
-            $resultado = pedidos::select('id', 'usuario_id', 'cliente_id', 'link', 'valor_total', 'status_id', 'tipo_pagamento')->where('id', $id)->with('itensPedidos', function ($query) {
+            $resultado = pedidos::select('id', 'usuario_id', 'cliente_id',
+            'link', 'valor_total', 'status_id', 'tipo_pagamento')->where('id',
+            $id)->with('itensPedidos', function ($query) {
                 $query->select('id', 'produto_id', 'quantidade', 'preco');
             })->first();
             if (!$resultado) {
