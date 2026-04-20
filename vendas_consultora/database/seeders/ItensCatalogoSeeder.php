69d470aa62e86c33a1aa6f3296998bba4623949c
Alewesley-Sousa <alewesley1234@gmail.com>
2026-04-20 19:23:35 -0300

    [FIX] retirado o campo preco

--- vendas_consultora/database/seeders/ItensCatalogoSeeder.php
+++ vendas_consultora/database/seeders/ItensCatalogoSeeder.php
@@ -16,7 +16,6 @@ class ItensCatalogoSeeder extends Seeder
         $itensCatalogo = [
             // Catálogo Verão 2026 (catalogo_id = 1) - vendas
             [
-                'preco' => 39.90,
                 'pontos_necessarios' => null,
                 'status_id' => 1,
                 'estoque_disponivel' => 100,
@@ -24,7 +23,6 @@ class ItensCatalogoSeeder extends Seeder
                 'produto_id' => 1
             ],
             [
-                'preco' => 59.90,
                 'pontos_necessarios' => null,
                 'status_id' => 1,
                 'estoque_disponivel' => 80,
@@ -32,7 +30,6 @@ class ItensCatalogoSeeder extends Seeder
                 'produto_id' => 2
             ],
             [
-                'preco' => 89.90,
                 'pontos_necessarios' => null,
                 'status_id' => 2,
                 'estoque_disponivel' => 0,
@@ -40,7 +37,6 @@ class ItensCatalogoSeeder extends Seeder
                 'produto_id' => 3
             ],
             [
-                'preco' => 79.90,
                 'pontos_necessarios' => null,
                 'status_id' => 1,
                 'estoque_disponivel' => 50,
@@ -50,7 +46,6 @@ class ItensCatalogoSeeder extends Seeder
 
             // Catálogo Pontos Bronze (catalogo_id = 2) - recompensas
             [
-                'preco' => 39.90,
                 'pontos_necessarios' => 400,
                 'status_id' => 1,
                 'estoque_disponivel' => 100,
@@ -58,23 +53,20 @@ class ItensCatalogoSeeder extends Seeder
                 'produto_id' => 1
             ],
             [
-                'preco' => 59.90,
                 'pontos_necessarios' => 600,
                 'status_id' => 1,
                 'estoque_disponivel' => 80,
                 'catalogo_id' => 2,
                 'produto_id' => 2
             ],
-            [
-                'preco' => 129.90,
+            [,
                 'pontos_necessarios' => 1300,
                 'status_id' => 1,
                 'estoque_disponivel' => 40,
                 'catalogo_id' => 2,
                 'produto_id' => 7
             ],
-            [
-                'preco' => 139.90,
+            [,
                 'pontos_necessarios' => 1400,
                 'status_id' => 2,
                 'estoque_disponivel' => 0,
@@ -84,7 +76,6 @@ class ItensCatalogoSeeder extends Seeder
 
             // Catálogo Especial Dia das Mães (catalogo_id = 3) - vendas
             [
-                'preco' => 34.90,
                 'pontos_necessarios' => null,
                 'status_id' => 1,
                 'estoque_disponivel' => 120,
@@ -92,7 +83,6 @@ class ItensCatalogoSeeder extends Seeder
                 'produto_id' => 9
             ],
             [
-                'preco' => 36.90,
                 'pontos_necessarios' => null,
                 'status_id' => 1,
                 'estoque_disponivel' => 110,
@@ -100,7 +90,6 @@ class ItensCatalogoSeeder extends Seeder
                 'produto_id' => 10
             ],
             [
-                'preco' => 49.90,
                 'pontos_necessarios' => null,
                 'status_id' => 2,
                 'estoque_disponivel' => 0,
@@ -108,7 +97,6 @@ class ItensCatalogoSeeder extends Seeder
                 'produto_id' => 11
             ],
             [
-                'preco' => 12.90,
                 'pontos_necessarios' => null,
                 'status_id' => 1,
                 'estoque_disponivel' => 200,
